52fa7c585ca42cd6a5f9647ea5587710469af71e
WalkingPizza <kevin.iatauro@gmail.com>
2022-04-01 01:21:32 +0200

    Handle media replacement

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -3,14 +3,17 @@
 const { getService, isValidMimeType } = require('./utils');
 
 module.exports = ({ strapi }) => {
-  /* Generate a placeholder when a new image is uploaded */
+  /* Generate a placeholder when a new image is uploaded or updates */
+
+  const generatePlaceholder = async (event) => {
+    const { data } = event.params;
+    if (!isValidMimeType(data.mime)) return;
+    data.placeholder = await getService(strapi, 'placeholder').generate(data.url);
+  };
 
   strapi.db.lifecycles.subscribe({
     models: ['plugin::upload.file'],
-    async beforeCreate(event) {
-      const { data } = event.params;
-      if (!isValidMimeType(data.mime)) return;
-      data.placeholder = await getService(strapi, 'placeholder').generate(data.url);
-    },
+    beforeCreate: generatePlaceholder,
+    beforeUpdate: generatePlaceholder,
   });
 };
