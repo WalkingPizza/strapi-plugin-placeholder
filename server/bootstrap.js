6732cd8e17d059cc6ee0549ac4c66f15fdc33707
WalkingPizza <kevin.iatauro@gmail.com>
2022-08-26 19:10:56 +0200

    Fix placeholder gen when MIME is missing

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,13 +1,13 @@
 'use strict';
 
-const { getService, isValidMimeType } = require('./utils');
+const { getService, canGeneratePlaceholder } = require('./utils');
 
 module.exports = ({ strapi }) => {
   /* Generate a placeholder when a new image is uploaded or updates */
 
   const generatePlaceholder = async (event) => {
     const { data } = event.params;
-    if (!isValidMimeType(data.mime)) return;
+    if (!canGeneratePlaceholder(data)) return;
     data.placeholder = await getService(strapi, 'placeholder').generate(data.url);
   };
 
--- server/utils.js
+++ server/utils.js
@@ -1,14 +1,18 @@
 'use strict';
 
 const pluginId = require('./plugin-id');
+const mimeTypes = require('mime-types');
 
 /**
- * Checks whether the passed MIME type is supported by the plugin, hence whetehr a placeholder can be generated.
- * @param {string} mimeType the MIME type of the file we want to generate a placeholder for
- * @returns {boolean} whether a placeholder can be generated for the given MIME type
+ * Checks whether the passed file has a MIME type that is supported by the plugin, hence whether a placeholder can be generated.
+ * @param {string} file the file we want to generate a placeholder for
+ * @returns {boolean} whether a placeholder can be generated for the given file
  */
 
-const isValidMimeType = (mimeType) => mimeType.startsWith('image/');
+const canGeneratePlaceholder = (file) => {
+  if (!file.mime) file.mime = mimeTypes.lookup(file.name);
+  return file.mime?.startsWith('image/');
+};
 
 /**
  * Helper that retrieves one of the available services of this plugin from Strapi.
@@ -19,6 +23,6 @@ const isValidMimeType = (mimeType) => mimeType.startsWith('image/');
 const getService = (strapi, service) => strapi.plugin(pluginId).service(service);
 
 module.exports = {
-  isValidMimeType,
+  canGeneratePlaceholder,
   getService,
 };
