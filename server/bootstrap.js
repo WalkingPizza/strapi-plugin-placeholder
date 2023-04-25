bc34a712ff8d6033fbe1a3305009cf2a775636e7
Daniel Masterson <daniel.masterson@novadawnstudios.co.uk>
2023-04-25 12:44:25 +0100

    Fix plugin not updating placeholder on file update

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -6,7 +6,16 @@ module.exports = ({ strapi }) => {
   /* Generate a placeholder when a new image is uploaded or updates */
 
   const generatePlaceholder = async (event) => {
-    const { data } = event.params;
+    const { data, where } = event.params;
+
+    if(!data.url || !data.mime) {
+      // If the returned data is missing a url or mime property (probably because we're doing an update)
+      // then we'll need to pull these values from the upload.file plugin and merge them in.
+      const file = await strapi.entityService.findOne("plugin::upload.file", where.id);
+      data.url = data.url ?? file.url;
+      data.mime = data.mime ?? file.mime;
+    }
+
     if (!canGeneratePlaceholder(data)) return;
     data.placeholder = await getService(strapi, 'placeholder').generate(data.url);
   };
